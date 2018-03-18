6dc01eff29204d1c85642cd301d05e8aa64ff1eb
BABA <mr.ufukgurbuz@gmail.com>
2018-03-19 02:23:26 +0300

    v7 : Update new remote database URI

--- twCollector/mongoutils.py
+++ twCollector/mongoutils.py
@@ -1,9 +1,12 @@
 
+import os
 import pymongo
 
 # MongoLab Remote Database URI
-DBPATH = "mongodb://baba44:murat_44@data-science-database-shard-00-00-1ef45.mongodb.net:27017,data-science-database-shard-00-01-1ef45.mongodb.net:27017,data-science-database-shard-00-02-1ef45.mongodb.net:27017/test?ssl=true&replicaSet=data-science-database-shard-0&authSource=admin" # "mongodb://baba44:murat_44@ds151908.mlab.com:51908/data-science-database" # "mongodb://localhost:27017/"
-db_name = 'Twitter'
+#DBPATH = "mongodb://baba44:murat_44@data-science-database-shard-00-00-1ef45.mongodb.net:27017,data-science-database-shard-00-01-1ef45.mongodb.net:27017,data-science-database-shard-00-02-1ef45.mongodb.net:27017/test?ssl=true&replicaSet=data-science-database-shard-0&authSource=admin" # "mongodb://baba44:murat_44@ds151908.mlab.com:51908/data-science-database" # "mongodb://localhost:27017/"
+#DBPATH = "mongodb://heroku_k7jxsfsz:fgistv9a346c6l26sdppvhobmq@ds115749.mlab.com:15749/heroku_k7jxsfsz"
+DBPATH = os.environ['MONGOLAB_BLACK_URI']
+db_name = 'heroku_k7jxsfsz'
 collection_name= 'Tweets'
 
 class MongodbClient:
